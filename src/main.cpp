227c9a024dfc8926fbd5ff81b4ab709fe942b8c7
byt3f1d <bytefid@gmail.com>
2026-07-06 17:12:45 +0300

    update main.cpp for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <print>
+#include "metadata_parser/MetadataParser.h"
 
 int main() {
     printf("Hello World!\n");
+    IOManager io_manager;
+    const auto metadata_result = io_manager.LoadMetadata("meta_raw.dat");
+    if (!metadata_result.has_value()) {
+        return 1;
+    }
+    const auto& metadata = metadata_result.value();
+
+    MetadataParser parser(io_manager, metadata);
+
+    const auto fields_result = parser.ParseFields(380);
+    if (!fields_result.has_value()) {
+        return 1;
+    }
+
+    const auto& fields = fields_result.value();
+    std::print("Parsed {} fields successfully.\n", fields.size());
 
     return 0;
 }
