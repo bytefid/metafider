070c7dcd4675187aa93f0ced49d44752bbb84de0
byt3f1d <bytefid@gmail.com>
2026-07-07 12:00:44 +0300

    update main.cpp for tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <print>
+
+#include "config/ConfigLoader.h"
 #include "metafider_app/MetafiderApp.h"
 
 int main() {
+    const auto config_result = metafider::config::LoadConfig("config.json");
+    if (!config_result.has_value()) {
+        std::print("Failed to load config: {}\n", IO::ToCString(config_result.error()));
+        return 1;
+    }
+    const auto& config = config_result.value();
+
     MetafiderApp main_app;
-    if (!main_app.Init()) {
+    if (!main_app.Init(config.offset_heuristics)) {
         return 1;
     }
 
-    if (!main_app.Load("meta_raw.dat")) {
+    if (!main_app.Load(config.metadata_path)) {
         return 1;
     }
 
-    main_app.Parse();
-
+    main_app.Parse(config.header_size);
     main_app.PrintData();
 
     return 0;
